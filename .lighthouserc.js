6dd1bd60d0d61b857b6b1b58f6c058511ce9a66f
dnr14 <dnr14@naver.com>
2022-07-24 23:42:43 +0900

    chore : lighthouse ci setting

---
+++ .lighthouserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    ci:{
+        collect:{
+            staticDistDir:"./build",
+            url:["http://localhost:3000"],
+            numberOfRuns:1,
+        },
+        upload: {
+            target: "temporary-public-storage",
+        },
+        assert: {
+             assertions : {
+                "categories:performance": ["warn", { "minScore": 0.9 }],
+                "categories:accessibility": ["error", { "minScore": 1 }]
+            }
+        }
+    }
+}
