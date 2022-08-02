4a99408e31142d3ce75b4f209b9c14d88ed8cbfc
dnr14 <dnr14@naver.com>
2022-08-03 01:35:26 +0900

    feat : 테스트를 위한 페이지 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,7 @@
-import {Link} from "react-router-dom";
 
 const Home = () => {
     return (
-        <div>
-            home
-            <div><Link to={"/home"}>홈</Link></div>
-            <div><Link to={"/login"}>로그인</Link></div>
-        </div>
+        <div>home</div>
     );
 };
 
