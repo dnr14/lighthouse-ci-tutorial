7ef5654d120a9c875af5117450334d834f69d95d
dnr14 <dnr14@naver.com>
2022-08-21 23:49:49 +0900

    styled : Nav컴포넌트 스타일 변경

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,25 @@
+import { styled } from "@stitches/react";
 import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav>
+    <StyledNav>
       <Link href="/home">Home</Link>
       <Link href="/login">login</Link>
-    </nav>
+    </StyledNav>
   );
 }
+
+const StyledNav = styled("nav", {
+  display: "flex",
+  gap: 10,
+  "& > a": {
+    padding: 7.5,
+    border: "1px solid #ccc",
+    borderRadius: 5,
+    cursor: "pointer",
+    textDecoration: "none",
+  },
+  "& > a:visited": { color: "#2c7fff" },
+  "& > a:hover": { background: "#2c7fff", color: "#fff" },
+});
